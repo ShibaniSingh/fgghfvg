89cd0d9a6bcf08796b58f856aa2106e5b33b511c
ShibaniSingh <shibani888ksriet@gmail.com>
2016-07-22 15:53:19 +0530

    Add files via upload

---
+++ JavaApplication6.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.math.*;
+
+/**
+ *
+ * @author mural
+ */
+public class JavaApplication6 {
+
+    
+    public static void main(String args[]) {
+          Scanner sc = new Scanner(System.in);
+ 
+        System.out.print("Enter a string: ");
+        String st = sc.nextLine();
+ 
+        String word = "";
+        String largeWord = "";
+        int i, l, lw, max = 0, c = 0;
+        char ch;
+        st = st + " ";
+        l = st.length();
+        for (i = 0; i < l; i++) {
+            ch = st.charAt(i);
+            if (ch != ' ') {
+                word = word + ch;
+            } else {
+                lw = word.length();
+                if (lw > max) {
+                    max = lw;
+                    largeWord = word;
+                }
+                word = "";
+            }
+        }
+        System.out.println("Largest Word: " + largeWord);    
+}
+}
